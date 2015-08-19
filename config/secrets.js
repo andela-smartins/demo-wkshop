967fe2c81612a5707b88565a5806787ee4a2806a
Seun Martins <impactmass2000@gmail.com>
2015-08-19 07:40:30 +0100

    add replica db config

--- config/secrets.js
+++ config/secrets.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017,localhost:27018,localhost:27019/demo',
 
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
