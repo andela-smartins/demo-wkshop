c0228c4de02ae601b2cca2d440efbc7a123209b2
Seun Martins <impactmass2000@gmail.com>
2015-08-19 07:34:02 +0100

    db setup

--- config/secrets.js
+++ config/secrets.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/demo',
 
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
